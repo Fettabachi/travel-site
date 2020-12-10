1257ae03b50a6c304f2ad09321690a003ac6036f
Tim <timfettermail@gmail.com>
2020-12-10 06:36:43 -0500

    Render the secret area only if running the app on Netlify.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,20 +2,24 @@ import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
-import ClientArea from "./modules/ClientArea"
 
-new ClientArea()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 new MobileMenu();
 new StickyHeader();
 let modal;
 
+if (document.URL.indexOf("zen-meitner-405009.netlify.app") >= 0) {
+  import("./modules/ClientArea").then((x) => {
+    ClientArea = new x.default();
+  });
+}
+
 document.querySelectorAll(".open-modal").forEach((el) => {
   el.addEventListener("click", (e) => {
     e.preventDefault();
     if (typeof modal == "undefined") {
-      import(/* webpackChunkName: "modal" */"./modules/Modal")
+      import(/* webpackChunkName: "modal" */ "./modules/Modal")
         .then((x) => {
           modal = new x.default();
           setTimeout(() => modal.openTheModal(), 20);
